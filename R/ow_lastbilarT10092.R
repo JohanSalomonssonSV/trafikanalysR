a2babafd1aeb4da54b22b69f98e23c6d065bfa8f
JohanSalomonssonSV <61935963+JohanSalomonssonSV@users.noreply.github.com>
2021-06-04 12:06:16 +0200

    New functions

---
+++ R/ow_lastbilarT10092.R
@@ -0,0 +1,42 @@
+#' @title ow_lastbilarT10092
+#' @importFrom magrittr "%>%"
+#' @importFrom readr read_csv parse_number
+#' @importFrom dplyr mutate filter
+#' @importFrom httr GET content
+#' @importFrom jsonlite fromJSON
+#' @importFrom forcats fct_reorder
+#' @importFrom plyr ldply
+#' @param URL url to API
+#' @export
+ow_lastbilarT10092 <- function(URL) {
+  Comb_selection <-
+    readr::read_csv("https://raw.githubusercontent.com/JohanSalomonssonSV/trafikanalysR/master/data/lastbilarT10092.csv")
+
+
+  Variable_df  <- lapply(unique(Comb_selection$Short), function(i) {
+    TABLE_OF_INTEREST <-
+      Comb_selection %>% dplyr::filter(Short == i) %>% .$Code_snippet
+    LINK <- paste0(URL, TABLE_OF_INTEREST)
+    raw_res <- httr::GET(LINK)
+    text_content <- httr::content(raw_res, as = "text")
+    text_content_json <- text_content %>% jsonlite::fromJSON()
+
+    temp_data <-
+      text_content_json$Header$Column[c("Name", "Value", "Type")] %>% dplyr::mutate(DATA =
+                                                                                      paste0("Data: ", i),
+                                                                                    InData =
+                                                                                      1) %>% dplyr::rename("Var_desc" = Value)
+    temp_data
+  })
+
+  t <- plyr::ldply(Variable_df, rbind)
+  t <- t %>% dplyr::mutate(
+    Var_desc = forcats::fct_reorder(Var_desc, InData, sum),
+    Name = forcats::fct_reorder(Name, InData, sum),
+    DATA = forcats::fct_reorder(DATA, -readr::parse_number(DATA))
+  )
+  t
+
+}
+
+
