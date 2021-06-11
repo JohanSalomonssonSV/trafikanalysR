89f0f1cc039778e7a9173daf91f77b7ef0f778a8
JohanSalomonssonSV <61935963+JohanSalomonssonSV@users.noreply.github.com>
2021-06-11 15:39:51 +0200

    sjofartstrafik

--- Slask_notUse.R
+++ Slask_notUse.R
@@ -1,2 +1,10 @@
 # sjofartsforetagEuT08092<-readxl::read_excel("~/Nystrom/svenskHandel/trafa_api/sjofartsforetagEuT08092.xlsx")
 # readr::write_csv(sjofartsforetagEuT08092,file = "C:/Users/johan/Documents/github/trafikanalysR/trafikanalysR/data/sjofartsforetagEuT08092.csv")
+
+
+# sjofartsforetagEuT08092<-readxl::read_excel("~/Nystrom/svenskHandel/trafa_api/sjofartsforetagEuT08092.xlsx")
+# readr::write_csv(sjofartsforetagEuT08092,file = "C:/Users/johan/Documents/github/trafikanalysR/trafikanalysR/data/sjofartsforetagEuT08092.csv")
+
+
+# sjotrafikT0802<-readxl::read_excel("~/Nystrom/svenskHandel/trafa_api/sjotrafikT0802.xlsx")
+# readr::write_csv(sjotrafikT0802,file = "C:/Users/johan/Documents/github/trafikanalysR/trafikanalysR/data/sjotrafikT0802.csv")
